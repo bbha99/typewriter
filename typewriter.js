c430bbdf1643ae5baf7015bbe077b279a49e8d65
bbha99 <branha99@gmail.com>
2023-01-17 15:52:17 -0800

    created a typewriter loop using setTimeout

---
+++ typewriter.js
@@ -0,0 +1,9 @@
+const sentence = "hello there from lighthouse labs\n";
+
+let delay = 50;
+for (const char of sentence) {
+  setTimeout(() => {
+    // print the char here
+    process.stdout.write(char);
+  }, delay += 50); // <= 1s delay to make it noticeable. Can dial it down later.
+}
